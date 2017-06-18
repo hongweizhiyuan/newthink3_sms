3312f46a70427e59e6191af2979d9e0b4c4148bb
子弹兄 <403236160@qq.com>
2017-06-18 15:21:09 +0800

    修改的w代码

--- Controller/SmsController.class.php
+++ Controller/SmsController.class.php
@@ -9,26 +9,27 @@ namespace Sms\Controller;
 use Common\Controller\FrontbaseController;
 
 class SmsController extends FrontbaseController{
-	/*发送短信*/
-	public function sms_send(){
-		require_once C(EXTEND_PATH).'Alidayu/TopClient.php';
-		require_once C(EXTEND_PATH).'Alidayu/ResultSet.php';
-		require_once C(EXTEND_PATH).'Alidayu/RequestCheckUtil.php';
-		require_once C(EXTEND_PATH).'Alidayu/TopLogger.php';
-		require_once C(EXTEND_PATH).'Alidayu/AlibabaAliqinFcSmsNumSendRequest.php';
-//		dump (class_exists(TopClient)); //检测是否有此类存在
 
-		$rand_number = randNum(6);
-		$sms_signName = "身份验证";
-		$sms_param = "{code:'".$rand_number."',product:'力拓'}";
-		$sms_recNum  = I('mobile');
-		$sms_templateCode = "SMS_60025694";
-		//将生成的验证码存入缓存中
+    /* 发送短信 */
+	public function sms_send(){
+
+        //step1:配置 已经写到了config.php 配置里了
+        $rand_number = randNum(6);
+		$sms_signName = C('SMS_SIGNNAME');
+		$sms_param = "{code:'".$rand_number."',product:'".C('SMS_PRODUCT')."'}";
+		$sms_templateCode = C('SMS_TEMPLATECODE');
+
+		//step2:将生成的验证码存入cookie中
 		cookie('verificationCode',$rand_number);
-		/*实例化Alidayu*/
+
+		//step3:获取手机号,并把手机号验证码存入cookie中
+        $sms_recNum  = I('mobile');
+        cookie('verificationMobile',$sms_recNum);
+
+		//step4:实例化Alidayu
 		$sms = new \TopClient();
-		$sms->appkey = "23736309";
-		$sms->secretKey = "aef7d68ab59d5278a35d586bd9546a35";
+		$sms->appkey = C('APPKEY');
+		$sms->secretKey = C('SECRETKEY');
 		$req = new \AlibabaAliqinFcSmsNumSendRequest();
 		$req->setSmsType("normal"); //这个不用改你短信的话就默认这个就好了
 		$req->setSmsFreeSignName($sms_signName); //这个是签名
@@ -38,9 +39,11 @@ class SmsController extends FrontbaseController{
 		$resp = $sms->execute($req);
 		$_success = $resp->result->success;
 		$_msg = $resp->sub_msg;
+
+		//step6:状态及输出
 		if($_success){
 			$arr['status'] = '1';
-			$arr['msg']='发送成功';
+			$arr['msg']='发送成功，验证码不要告诉其他人！';
 		}else if($_msg){
 			$arr['status'] = '0';
 			$arr['msg'] = '1分钟内只能发一条，一小时7条验证码';
@@ -51,20 +54,28 @@ class SmsController extends FrontbaseController{
 		
 		$this->output($arr);
 	}
-	/*短信验证码校验*/
+
+
+	/* 短信验证码校验 */
 	public function verification(){
-		//session中取出验证码
+
+		//step1:cookie中取出验证码手机号
 		$verificationCode = cookie('verificationCode');
-		//用户传递验证码
+        $verificationMobile = cookie('verificationMobile');
+
+		//step2:用户传递验证码
 		$verificationCodeCorr = I('code');
-		if($verificationCode == $verificationCodeCorr){
+        $verificationMobileCorr = I('mobile');
+
+        //step3:验证手机号和验证码状态及输出
+		if($verificationCode == $verificationCodeCorr && $verificationMobile==$verificationMobileCorr){
 			$arr['status'] = '1';
 			$arr['msg'] = '验证码校验成功';
-			$arr['code'] = $verificationCode;
 		} else {
 			$arr['status'] = '0';
 			$arr['msg'] = '验证码校验失败';
 		}
 		$this->output($arr);
 	}
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-#newthink3_sms
+# NewThink3 短信模块
+
+
+## （一）说明
+
+注：这里我们用的是“阿里大于”的短信`验证`模块儿！
+
+官方网站： https://www.alidayu.com
+
+
+## （二）依赖
+
+目前依赖于根目录下的文件 这五个文件！如果想开发更多功能，可参考API，这里我们目前没有特殊需求，就不加了！
+
+相关文件的说明文档都在官方的文档，请参考：https://api.alidayu.com/docs/api_list.htm?spm=a3142.7629065.4.3.SC2QiJ&cid=20711
+
+这里我们只用到了以下几个文件
+```
+extend
+	|- Alidayu
+		|-- TopClient.php
+		|-- ResultSet.php
+		|-- RequestCheckUtil.php
+		|-- TopLogger.php
+		|-- AlibabaAliqinFcSmsNumSendRequest.php			短信验证码申请
+```
+
+## （二）配置
+
+针对每一个项目，我对要在 `/data/conf/config.php` 文件配置增加如下参数：
+
+
+1.51聚配置参数
+
+```
+/* 短信配置 */
+"SMS_SIGNNAME" 		=> '身份验证',                          //名称：身份验证，不能改，一改就不能用了
+"SMS_PRODUCT" 		=> '51聚',                              //APP的名称
+"SMS_TEMPLATECODE" 	=> 'SMS_60025694',                     	//短信模板，一般不用改
+
+/* 短信安全配置 */
+"APPKEY" 			=> '23736309',                          //appkey
+"SECRETKEY" 		=> 'aef7d68ab59d5278a35d586bd9546a35',  //secretkey
+```
+
+2. 力拓配置参数
+
+```
+/* 短信配置 */
+"SMS_SIGNNAME" 		=> '身份验证',                          //名称：身份验证，不能改，一改就不能用了
+"SMS_PRODUCT" 		=> '力拓',                              //APP的名称
+"SMS_TEMPLATECODE" 	=> 'SMS_60025694',                     	//短信模板，一般不用改
+
+/* 短信安全配置 */
+"APPKEY" 			=> '24405529',                          //appkey
+"SECRETKEY" 		=> '2cfb981f9f5789d599199f733e37e57c',  //secretkey
+
+```
+
+一般我们修改的是 `SMS_PRODUCT` `APPKEY`  `SECRETKEY`，这三个配置参数，其他默认状态即可！
+
